2163c36ecb6df48bf745f1173526e498c08fd1d9
karuna24s <karuna24s@gmail.com>
2018-04-11 18:05:50 -0400

    setup first react component

---
+++ src/components/PlaylistsContainer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class PlaylistsContainer extends Component {
+  render() {
+    return (
+      <div>
+        Playlists
+      </div>
+    )
+  }
+}
+
+export default PlaylistsContainer
