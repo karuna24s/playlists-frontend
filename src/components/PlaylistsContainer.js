3b14f3928604c5a4a60d88a29de92c115dd0a33c
karuna24s <karuna24s@gmail.com>
2018-04-11 20:57:26 -0400

    mplement adding api end point for creating a new playlist

--- src/components/PlaylistsContainer.js
+++ src/components/PlaylistsContainer.js
@@ -1,38 +1,62 @@
 import React, { Component } from 'react'
-import Playlist from './Playlist'
 import axios from 'axios'
+import Playlist from './Playlist'
+import update from 'immutability-helper'
 
 class PlaylistsContainer extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      playlists: []
+      playlists: [],
+      editingPlaylistId: null
     }
   }
 
   componentDidMount() {
-     axios.get('http://localhost:3001/api/v1/playlists.json')
-     .then(response => {
-       console.log(response)
-       this.setState({playlists: response.data})
-     })
-     .catch(error => console.log(error))
-   }
+    axios.get('http://localhost:3001/api/v1/playlists.json')
+    .then(response => {
+      console.log(response)
+      this.setState({playlists: response.data})
+    })
+    .catch(error => console.log(error))
+  }
 
+  addNewPlaylist = () => {
+    axios.post(
+      'http://localhost:3001/api/v1/playlists',
+      { playlist:
+        {
+          title: '',
+          body: ''
+        }
+      }
+    )
+    .then(response => {
+      console.log(response)
+      const playlists = update(this.state.playlists, {
+        $splice: [[0, 0, response.data]]
+      })
+      this.setState({
+        playlists: playlists,
+        editingPlaylistId: response.data.id
+      })
+    })
+    .catch(error => console.log(error))
+  }
 
   render() {
     return (
       <div>
         <div>
-          <button className="newPlaylistButton">
+          <button className="newPlaylistButton"  onClick={this.addNewPlaylist} >
             New Playlist
           </button>
         </div>
         {this.state.playlists.map((playlist) => {
-          return(<Playlist playlist={playlist} key={playlist.id} />)
+          return (<Playlist playlist={playlist} key={playlist.id} />)
         })}
       </div>
-    )
+    );
   }
 }
 
