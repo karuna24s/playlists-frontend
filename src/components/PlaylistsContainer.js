b1d5a69475ca7ce47dc218c82a146654e85af4f7
karuna24s <karuna24s@gmail.com>
2018-04-11 20:00:01 -0400

    fetch api data with axios

--- src/components/PlaylistsContainer.js
+++ src/components/PlaylistsContainer.js
@@ -1,10 +1,35 @@
 import React, { Component } from 'react'
+import axios from 'axios'
 
 class PlaylistsContainer extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      playlists: []
+    }
+  }
+
+  componentDidMount() {
+     axios.get('http://localhost:3001/api/v1/playlists.json')
+     .then(response => {
+       console.log(response)
+       this.setState({playlists: response.data})
+     })
+     .catch(error => console.log(error))
+   }
+
+
   render() {
     return (
       <div>
-        Playlists
+        {this.state.playlists.map((playlist) => {
+          return(
+            <div className="tile" key={playlist.id} >
+              <h4>{playlist.title}</h4>
+              <p>{playlist.body}</p>
+            </div>
+          )
+        })}
       </div>
     )
   }
